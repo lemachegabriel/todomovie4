9f6e7c0764d510dd1743cd93c3092f51ce438b9e
lemachegabriel <lemache35@gmail.com>
2022-02-04 13:45:18 -0300

    Initial Commit

---
+++ todomovie4/todomovie4App.swift
@@ -0,0 +1,17 @@
+//
+//  todomovie4App.swift
+//  todomovie4
+//
+//  Created by Gabriel Lemache Castro on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct todomovie4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
